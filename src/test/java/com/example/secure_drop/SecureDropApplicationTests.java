1cdb19f06c54a9b8485eb2f94a57d64ff070c121
codingstreams <akxhdev@outlook.com>
2026-02-27 08:46:09 +0530

    init commit

---
+++ src/test/java/com/example/secure_drop/SecureDropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secure_drop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
